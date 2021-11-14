99e0405fe39484a1abf931ab2b5a3f165268836d
a87146423 <51416119+a87146423@users.noreply.github.com>
2021-11-14 08:19:29 +0800

    Update MembershipManage.py

--- private_cogs/MembershipManage.py
+++ private_cogs/MembershipManage.py
@@ -53,10 +53,10 @@ class MembershipManage(commands.Cog):
                 member = guild.get_member(item['Discord UID'])
                 await member.add_roles(role)
                 await member.send(
-                    '''[CN] 您的會員證明已被驗證，現在可以使用以下會限頻道了！'''
-                    '''[EN] Your proof have been verified and you can now access to the following membership channel.'''
-                    '''------------------------------------------------------'''
-                    '''<#803473713369710653>'''
+                    '''[CN] 您的會員證明已被驗證，現在可以使用以下會限頻道了！\n'''
+                    '''[EN] Your proof have been verified and you can now access to the following membership channel.\n'''
+                    '''------------------------------------------------------\n'''
+                    '''<#803473713369710653>\n'''
                     '''<#851664375319494676>'''
                     )
                 ws.update_value(f'K{index + 2}', 'Y')
