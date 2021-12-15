c313eeddcf6ccbdc99228d558ce512b3e1c86903
a87146423 <51416119+a87146423@users.noreply.github.com>
2021-12-16 07:52:47 +0800

    Update Restart.py

--- cogs/Restart.py
+++ cogs/Restart.py
@@ -12,15 +12,7 @@ class Restart(commands.Cog):
         TOKEN = os.environ.get("DC_TOKEN")
         await self.bot.close()
         await self.bot.start(TOKEN, reconnect=True)
-
-    @commands.slash_command(name='xratelimit', description='Check x-RateLimit.')
-    @commands.has_permissions(ban_members=True)
-    async def _check_x_ratelimit(self, inter):
-        r = requests.head(url="https://discord.com/api/v1")
-        try:
-            await inter.response.send_message(f"Rate limit {int(r.headers['Retry-After']) / 60} minutes left", ephemeral=True)
-        except:
-            await inter.response.send_message(f"No rate limit, Remaining: {int(r.headers['X-RateLimit-Remaining']) / 60}", ephemeral=True)
+        await inter.response.send_message(f"Bot restarted.")
 
 def setup(bot):
     bot.add_cog(Restart(bot))
